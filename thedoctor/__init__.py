202e83d4263969ed7a4d83991c44350c70acaf68
Dan Obermiller <dannnno16@gmail.com>
2015-03-23 09:33:46 -0700

    Made pep8 complient

--- thedoctor/__init__.py
+++ thedoctor/__init__.py
@@ -1,38 +1,45 @@
-from functools import wraps
 import inspect
-import warnings
 import logging
 import os
+import warnings
+from functools import wraps
+
 
 logger = logging.getLogger(__name__)
+
+
 class ValidationError(Exception):
     pass
+
 
 def arg_dict(func, args, kwargs):
     """Given a function and the args/kwargs that it is called with,
     construct the dictionary mapping arument names to argument values
     """
+
+    skip_validation = \
+        "skipping validation, %s will throw an exception" % func.__name__
     all_args = {}
     arg_names, vararg_name, keyword_name, defaults = inspect.getargspec(func)
     arg_names_set = set(arg_names)
     for name, value in zip(arg_names, args):
         all_args[name] = value
-    #handle keyword arguments
+    # handle keyword arguments
     for name, value in kwargs.items():
         if name in all_args:
             warnings.warn("duplicate value for %s" % name)
-            warnings.warn("skipping validation, %s will thrown an exception" % func.__name__)
+            warnings.warn(skip_validation)
             return None
 
         if name not in arg_names_set and not keyword_name:
             warnings.warn("function does not take parameter %s" % name)
-            warnings.warn("skipping validation, %s will thrown an exception" % func.__name__)
+            warnings.warn(skip_validation)
             return None
         if name not in arg_names_set and keyword_name:
             continue
         all_args[name] = value
 
-    #populate defaults
+    # populate defaults
     if defaults is not None:
         for name, value in zip(arg_names[-len(defaults):], defaults):
             if name not in all_args:
@@ -40,29 +47,29 @@ def arg_dict(func, args, kwargs):
 
     if len(args) > len(arg_names) and not vararg_name:
         warnings.warn("improper number of args")
-        warnings.warn("skipping validation, %s will thrown an exception" % func.__name__)
+        warnings.warn(skip_validation)
         return
 
     if set(all_args.keys()) != arg_names_set:
         warnings.warn("improper number of args")
-        warnings.warn("skipping validation, %s will thrown an exception" % func.__name__)
+        warnings.warn(skip_validation)
         return
 
-    #handle *args
+    # handle *args
     if vararg_name:
         varargs = args[len(arg_names):]
         all_args[vararg_name] = varargs
 
-    #handle **kwargs
+    # handle **kwargs
     if keyword_name:
         kw_args = {}
-        for k,v in kwargs.items():
+        for k, v in kwargs.items():
             if k not in all_args:
                 kw_args[k] = v
         all_args[keyword_name] = kw_args
 
-
     return all_args
+
 
 def _postprocess(obj):
     """We support convenient ways of specifying dependencies.
@@ -86,8 +93,10 @@ def _postprocess(obj):
             raise ValidationError("unknown specification %s" % spec)
     return callbacks
 
+
 def niceprint(obj, limit=1000):
     return str(obj)[:limit]
+
 
 def run_validate(func, arg, msg, msg_params):
     try:
@@ -97,16 +106,19 @@ def run_validate(func, arg, msg, msg_params):
         logger.exception(e)
         raise
 
+
 def _dict_validate(validators, inputs, msg):
     """validators - dict of validator function lists
     msg - logger message, containing 2 %s for k,v
     """
-    for k,v in validators.items():
+    for k, v in validators.items():
         for validator in v:
             value = inputs.get(k)
             run_validate(validator, value, msg, (k, niceprint(value)))
 
+
 def validate(**validators):
+
     def passthrough(func):
         return func
     if os.environ.get('NO_DOCTOR'):
@@ -116,15 +128,18 @@ def validate(**validators):
         validators[k] = _postprocess(validators[k])
     all_validator = validators.pop('_all', [])
     return_validator = validators.pop('_return', [])
+
     def decorator(func):
-        dict_error_message = "Error validating arg: %%s, value: %%s, on function %s"
+        dict_error_message = \
+            "Error validating arg: %%s, value: %%s, on function %s"
         dict_error_message = dict_error_message % func.__name__
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             all_args = arg_dict(func, args, kwargs)
             if all_args is None:
-                #should return an exception
-                #all_args is None if the call sig is wrong
+                # should return an exception
+                # all_args is None if the call sig is wrong
                 return func(*args, **kwargs)
             _dict_validate(validators, all_args, dict_error_message)
             for validator in all_validator:
@@ -133,9 +148,10 @@ def validate(**validators):
                              (func.__name__,))
             retval = func(*args, **kwargs)
             for validator in return_validator:
-                run_validate(validator, retval,
-                             "Error validating return value of %s, on function %s",
-                             (niceprint(retval), func.__name__))
+                run_validate(
+                    validator, retval,
+                    "Error validating return value of %s, on function %s",
+                    (niceprint(retval), func.__name__))
             return retval
         return wrapper
     return decorator
--- thedoctor/tests/test_integration.py
+++ thedoctor/tests/test_integration.py
@@ -1,27 +1,30 @@
-from .. import validate
 from .utils import raises
 from .. import ValidationError
-from ..validators import true, dict_validator
+from .. import validate
+from ..validators import dict_validator, true
+
 
 def test_integration():
     @validate(a=int, b=int)
-    def func(a,b):
-        return (a,b)
-    assert func(1,2) == (1,2)
+    def func(a, b):
+        return (a, b)
+    assert func(1, 2) == (1, 2)
     assert raises(ValidationError, func, 1, 'a')
 
     @validate(a=int)
-    def func(a,b):
-        return (a,b)
-    assert func(1,'a') == (1,'a')
+    def func(a, b):
+        return (a, b)
+    assert func(1, 'a') == (1, 'a')
     assert raises(ValidationError, func, 'a', 1)
+
 
 def test_return_validator():
     def return_validator(result):
         if result != 3:
             raise ValidationError('not 3')
+
     @validate(_return=[int, return_validator])
-    def func(a,b):
+    def func(a, b):
         return a + b
     assert raises(ValidationError, func, 1, 3)
     assert raises(ValidationError, func, 'a', 'b')
@@ -30,32 +33,37 @@ def test_return_validator():
     def sums_to_3(all_args):
         if all_args['a'] + all_args['b'] != 3:
             raise ValidationError('not sum to 3')
+
     @validate(_all=sums_to_3)
-    def func(a,b):
+    def func(a, b):
         return a + b
     assert raises(ValidationError, func, 1, 3)
     assert func(1, 2) == 3
 
 
 def test_lambda_validator():
-    @validate(_all=lambda x : true(x['a'] + x['b'] == 3, "must sum to 3"))
-    def func(a,b):
+    @validate(_all=lambda x: true(x['a'] + x['b'] == 3, "must sum to 3"))
+    def func(a, b):
         return a + b
     assert raises(ValidationError, func, 1, 3)
     assert func(1, 2) == 3
 
+
 def instance_method_test():
     class Test(object):
-        @validate(_return=lambda x : true(x % 2 == 0, "return must be even"), a=int, b=int)
+        @validate(_return=lambda x: true(x % 2 == 0, "return must be even"),
+                  a=int, b=int)
         def func(self, a, b):
             return a + b
     t = Test()
     assert raises(ValidationError, t.func, 1, 2)
-    t.func(2,4)
+    t.func(2, 4)
+
 
 def dict_validator_integration_test():
-    @validate(x=dict_validator({'name' : lambda x : true(x == 'sally', 'must be sally')}))
+    @validate(x=dict_validator(
+        {'name': lambda x: true(x == 'sally', 'must be sally')}))
     def func(x):
         return x
-    assert raises(ValidationError, func, {'name' : 'bob'})
-    func({'name' : 'sally'})
+    assert raises(ValidationError, func, {'name': 'bob'})
+    func({'name': 'sally'})
