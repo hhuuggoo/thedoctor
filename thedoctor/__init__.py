adf99ebed79b769dba47ead8d8a5d8ddc9513aa0
hugo <hugo>
2018-04-28 17:42:44 -0400

    switched from my arg_dict to inspect.signature

--- thedoctor/__init__.py
+++ thedoctor/__init__.py
@@ -12,65 +12,6 @@ class ValidationError(Exception):
     pass
 
 
-def arg_dict(func, args, kwargs):
-    """Given a function and the args/kwargs that it is called with,
-    construct the dictionary mapping arument names to argument values
-    """
-
-    skip_validation = \
-        "skipping validation, %s will throw an exception" % func.__name__
-    all_args = {}
-    arg_names, vararg_name, keyword_name, defaults = inspect.getargspec(func)
-    arg_names_set = set(arg_names)
-    for name, value in zip(arg_names, args):
-        all_args[name] = value
-    # handle keyword arguments
-    for name, value in kwargs.items():
-        if name in all_args:
-            warnings.warn("duplicate value for %s" % name)
-            warnings.warn(skip_validation)
-            return None
-
-        if name not in arg_names_set and not keyword_name:
-            warnings.warn("function does not take parameter %s" % name)
-            warnings.warn(skip_validation)
-            return None
-        if name not in arg_names_set and keyword_name:
-            continue
-        all_args[name] = value
-
-    # populate defaults
-    if defaults is not None:
-        for name, value in zip(arg_names[-len(defaults):], defaults):
-            if name not in all_args:
-                all_args[name] = value
-
-    if len(args) > len(arg_names) and not vararg_name:
-        warnings.warn("improper number of args")
-        warnings.warn(skip_validation)
-        return
-
-    if set(all_args.keys()) != arg_names_set:
-        warnings.warn("improper number of args")
-        warnings.warn(skip_validation)
-        return
-
-    # handle *args
-    if vararg_name:
-        varargs = args[len(arg_names):]
-        all_args[vararg_name] = varargs
-
-    # handle **kwargs
-    if keyword_name:
-        kw_args = {}
-        for k, v in kwargs.items():
-            if k not in all_args:
-                kw_args[k] = v
-        all_args[keyword_name] = kw_args
-
-    return all_args
-
-
 def _postprocess(obj):
     """We support convenient ways of specifying dependencies.
     field=[func1, func2, func3]
@@ -133,10 +74,10 @@ def validate(**validators):
         dict_error_message = \
             "Error validating arg: %%s, value: %%s, on function %s"
         dict_error_message = dict_error_message % func.__name__
-
+        sig = inspect.signature(func)
         @wraps(func)
         def wrapper(*args, **kwargs):
-            all_args = arg_dict(func, args, kwargs)
+            all_args = sig.bind(*args, **kwargs).arguments
             if all_args is None:
                 # should return an exception
                 # all_args is None if the call sig is wrong
